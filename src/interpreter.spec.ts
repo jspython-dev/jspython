cf4a17cface789db211681741a91018b6a9287b4
Pavlo Paska <ppaska@gmail.com>
2020-12-05 17:05:47 +0000

    added arithmetic priorities and parenthesis

--- src/interpreter.spec.ts
+++ src/interpreter.spec.ts
@@ -24,4 +24,24 @@ describe('Interpreter', () => {
     expect(await e.evaluate(`"'hello'" + " " + '"world"'`)).toBe(`'hello' "world"`);
   });
 
+  it('1+2*3', async () => {
+    expect(await e.evaluate("1+2*3")).toBe(7);
+    expect(await e.evaluate("1+2*3+4")).toBe(11);
+    expect(await e.evaluate("1+2*3+4*5")).toBe(27);
+  });
+
+  it('(1 + 2) * 3', async () => {
+    expect(await e.evaluate("(1 + 2) * 3")).toBe(9);
+    expect(await e.evaluate("(1 + 2) * 3 + 5")).toBe(14);
+  });
+
+  it('(1 + 2) * (3 + 5)', async () => {
+    expect(await e.evaluate("(1 + 2) * (3 + 5)")).toBe(24);
+
+    expect(await e.evaluate("2*(3+4)")).toBe(14);
+    expect(await e.evaluate("2*(3+4)+5")).toBe(19);
+    expect(await e.evaluate("2*(3+4)+5*6")).toBe(44);
+  });
+
+//(1 + 2) * (3 + 4) -5
 });
--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -2,12 +2,21 @@ import { BinOpNode, ConstNode, Ast, Token, ParserOptions, AstNode, OperatorsMap,
 
 export class Parser {
 
-    nextTokenOperatorIndex(tokens: Token[], cursor: number): number | null {
+    nextTokenOperatorIndex(tokens: Token[], cursor: number, endCursor: number): number | null {
         let i = cursor;
         let opToken = OperatorsMap[tokens[++i][0] as Operators]
         while (opToken === undefined) {
-            if (i + 1 >= tokens.length) { return null; }
-            opToken = OperatorsMap[tokens[++i][0] as Operators]
+            if (tokens[i][0] === '(') {
+                while (tokens[++i][0] !== ')') {
+                    if (i + 1 >= endCursor) {
+                        throw new Error(`Closing ')' is missing`);
+                    }
+                }
+            }
+
+            if (i + 1 >= endCursor) { return null; }
+            const token = tokens[++i];
+            opToken = OperatorsMap[token[0] as Operators]
         }
         return i;
     }
@@ -31,26 +40,69 @@ export class Parser {
         let node: AstNode | null = null;
         if (OperatorsMap[tokens[1][0] as Operators] === OperationTypes.Assignment) {
             const target = new SingleVarNode(tokens[0]);
-            const source = this.createBinOpModule(tokens, 2);
+            const source = this.createBinOpNode(tokens, 2, tokens.length);
             node = new AssignNode(target, source);
         } else {
-            node = this.createBinOpModule(tokens, cursor);
+            node = this.createBinOpNode(tokens, cursor, tokens.length);
         }
 
         ast.body.push(node)
         return ast;
     }
 
-    private createBinOpModule(tokens: Token[], startCursor: number): AstNode {
+    private createBinOpNode(tokens: Token[], startIndex: number, length: number): AstNode {
         let node: AstNode | null = null;
         do {
-            const left: AstNode = (node == null) ? new ConstNode(tokens[startCursor]) : node;
-            const op = tokens[++startCursor][0] as Operators;
-            const right = new ConstNode(tokens[++startCursor]);
+            const opIndex = this.nextTokenOperatorIndex(tokens, startIndex, length);
 
-            node = new BinOpNode(left, op, right);
+            if (opIndex === null) {
+                throw new Error('First operation is missing')
+            }
+            const op = tokens[opIndex][0] as Operators; // first operator
+
+            // next operator
+            const nextOpIndex = this.nextTokenOperatorIndex(tokens, opIndex, length);
+            const nextOp: Operators | null = (nextOpIndex !== null) ? tokens[nextOpIndex][0] as Operators : null;
+
+            if (tokens[startIndex][0] === '(') {
+                let ind = startIndex;
+                while (tokens[++ind][0] !== ')') {
+                    // check for end of array                    
+                }
+                node = this.createBinOpNode(tokens, startIndex + 1, ind);
+                startIndex += ind;
+
+            } else if (nextOp === '*' || nextOp === '/') {
+                // handle priority operators
+                const left: AstNode = (node == null) ? new ConstNode(tokens[startIndex]) : node;
+                const right: AstNode = this.createBinOpNode(tokens, startIndex + 2, startIndex + 5);
+                node = new BinOpNode(left, op, right);
+                startIndex += 4;
+            } else {
+                // normal case
+                const left: AstNode = (node == null) ? new ConstNode(tokens[startIndex]) : node;
+                const token = tokens[startIndex + 2];
+                let right: AstNode;
+                let offset = 2;
+
+                if (token[0] !== '(') {
+                    right = new ConstNode(token);
+                } else {
+                    let ind = startIndex;
+                    while (tokens[++ind][0] !== ')') {
+                        // check for end of array                    
+                    }
+
+                    right = this.createBinOpNode(tokens, startIndex + 2, ind);
+                    offset = ind;
+                }
+
+                node = new BinOpNode(left, op, right);
+                startIndex += offset;
+            }
         }
-        while (startCursor + 1 < tokens.length);
+        while (startIndex + 1 < length);
+
         return node;
     }
 }
